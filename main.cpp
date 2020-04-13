b1286006940333c7b8b2c3599cea2eed337c1d49
flyingsl0ths <randomemail32110@gmail.com>
2020-04-13 01:28:18 -0500

    Updated file

--- main.cpp
+++ main.cpp
@@ -1,7 +1,29 @@
 #include <iostream>
+#include <string.h>
+
+#define MAX_LENGTH 30
+
+void to_lowercase(char* word);
 
 int main()
 {
-	std::cout << "Heyo\n";
+	char word[MAX_LENGTH];
+
+	std::cout << "Enter a word: ";
+	std::cin >> word;
+
+	to_lowercase(word);
+
+	std::cout << word << '\n';
 	return 0;
+}
+
+void to_lowercase(char* word)
+{
+	int len = strlen(word);
+
+	for (int i = 0; i < len; ++i)
+	{
+		word[i] = tolower(word[i]);
+	}
 }
