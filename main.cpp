f25558a88f7cda68a1f09bc89d29fbff4af9d994
flyingsl0ths <randomemail32110@gmail.com>
2020-04-13 19:16:00 -0500

    Added to_lowercase() implementation

--- main.cpp
+++ main.cpp
@@ -1,9 +1,23 @@
 #include <iostream>
 #include <string>
+#include <string_view>
+#include <algorithm>
+
+namespace
+{
+std::string to_lowercase(std::string_view word)
+{
+	std::string lowercased = word.begin();
+	std::transform(word.begin(), word.end(), lowercased.begin(), tolower);
+	return lowercased;
+}
+} // namespace
 
 int main()
 {
 	std::string_view const test = "Heyo\n";
-	std::cout << test;
+	std::string lowercased_test = to_lowercase(test);
+
+	std::cout << lowercased_test;
 	return 0;
 }
